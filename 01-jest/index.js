5608c445e23ba99d0286a3fa575c3edb2f23a738
TTiip <2711430287@qq.com>
2021-10-18 15:56:47 +0800

    jest 测试编写

--- 01-jest/index.js
+++ 01-jest/index.js
@@ -1,10 +1,9 @@
 const add = (a, b) => a + b
 
+const subtract = (a, b) => a - b
 
-module.exports = {
-	add
-}
-
+// module.exports = { add, subtract }
 export {
-	add
+	add,
+	subtract
 }
--- 01-jest/test/index.spec.js
+++ 01-jest/test/index.spec.js
@@ -1,7 +1,25 @@
-import { add } from '../index'
+import { add, subtract } from '../index'
 
-describe('01-add', () => {
-	test('add(1, 2) 等于 3', () => {
-		expect(add(1, 2)).toBe(3)
+describe('真测试', () => {
+  test('add(1, 2) = 3', () => {
+		const a = 1
+		const b = 2
+    expect(add(a, b)).toBe(3)
+  })
+})
+
+describe('假测试', () => {
+	test("不写 expect ", () => {
+		subtract()
+	})
+})
+
+describe('异步测试', () => {
+	test('异步add', () => {
+		const a = 3
+		const b = 7
+		setTimeout(() => {
+			expect(add(a, b)).toBe(10)
+		}, 1000)
 	})
 })
